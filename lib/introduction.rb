e8527fb9ee4c92a9aaec00c2386f539bdbadbb37
Elias Delgado <evdelgado99@gmail.com>
2020-08-07 19:31:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,8 @@
 # introduce Josh using a name that out uts a phrase with that name using string interpolation
 
 def introduction (phrase)
-  name = "Josh"
   phrase = "Hi my name is #{name}"
+  puts phrase
 end
+
+name = "Josh"
